cd8b34bb33be8222828c951996d7b1ac5ceaf9d8
amay-zingg <anastasiamay29@gmail.com>
2021-03-22 01:09:28 -0400

    complete animation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,14 +8,15 @@ window.onload = function() {
     tl.to("#title-container", 0.5, {ease: Power1.easeOut, scale: 0.5, top: 220}, "-=0.5");
     tl.to("#bottom-section", 0.5, {ease: Power1.easeOut, top: 475}, "-=0.5");
 
-    tl.to("#photo1", 2, {ease: Linear.easeNone, left: -300}, "-=0.5");
+    // tl.to("#photo1", 1, {opacity: 1, ease: Linear.easeIn});
+    tl.to("#photo1", 2, {ease: Linear.easeOut, left: -300}, "-=0.5");
     tl.to("#photo1", 0.4, {opacity: 0}, "-=0.4");
 
     tl.to("#photo2", 0.4, {opacity: 1}, "-=0.4");
-    tl.to("#photo2", 2, {ease: Linear.easeNone, left: 0}, "-=0.5");
+    tl.to("#photo2", 2, {ease: Linear.easeOut, left: 0}, "-=0.5");
 
     tl.to("#photo3", 0.4, {opacity: 1}, "-=0.4");
-    tl.to("#photo3", 2, {ease: Linear.easeNone, left: -300}, "-=0.5");
+    tl.to("#photo3", 2, {ease: Linear.easeOut, left: -300}, "-=0.5");
 
     tl.to("#photo4", 0.4, {opacity: 1}, "-=0.4");
     
